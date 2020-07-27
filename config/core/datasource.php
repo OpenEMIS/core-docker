d8f286ece30506d8fdc5a4f46bcd9b7acb77852f
Karl Turnbull <39050650+karlturnbull@users.noreply.github.com>
2020-07-27 13:25:32 +0800

    Add files via upload

---
+++ config/core/datasource.php
@@ -0,0 +1,20 @@
+<?php
+return [
+    'Datasources' => [
+        'default' => [
+            'className' => 'Cake\Database\Connection',
+            'driver' => 'Cake\Database\Driver\Mysql',
+            'persistent' => false,
+            'host' => 'OE_HOST',
+            'port' => 'OE_PORT',
+            'username' => 'OE_USERNAME',
+            'password' => 'OE_PASSWORD',
+            'database' => 'OE_DATABASE',
+            'encoding' => 'utf8mb4',
+            'timezone' => 'UTC',
+            'cacheMetadata' => true,
+            'quoteIdentifiers' => true,
+            //'init' => ['SET GLOBAL innodb_stats_on_metadata = 0'],
+        ],
+    ],
+];
